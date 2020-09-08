657cb9a99955a54eb2a3ba0fe6fdcbf88a404aca
Hilderal Térence <terencehilderal@gmail.com>
2020-09-08 19:15:41 +0200

    commence les routes pour les commentaires , post com Ok , reste delete et getAll

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -1,22 +1,35 @@
 // import
 
-const models = require('../models');
-const fs = require('fs')
+const models = require("../models");
+const fs = require("fs");
 
-exports.createComment = (req, res) => {
-
-  models.Comment.create({
-    comments: req.body.comments,
-    UserId: req.user.id,
-    PostId: req.params.id
-  })
-    .then((comment) => res.status(200).json({ message: ' Your comment has been sent!', comment }))
-    .catch(err => res.status(500).json({ err }))
-
+exports.createComment = async (req, res) => {
+	try {
+		const newCom = await models.Comment.create({
+			comments: req.body.comments,
+			UserId: req.user.id,
+			PostId: req.params.id
+		});
+		if (newCom) {
+			res.status(201).json({ message: "Your comment has been sent", newCom });
+		} else {
+			throw new Error({ error: "Sorry , something gone wrong" });
+		}
+	} catch (err) {
+		res.status(400).json({ error: "Sorry we couldn't create your comment" });
+	}
 };
 
+exports.getComment = async (req, res) => {
+	try {
+		const comments = await models.Comment.findAll({
+			include: [models.User],
+			order: ["createdAt", "DESC"]
+		});
+		res.status(200).send(comments);
+	} catch (error) {
+		res.status(400).send(error);
+	}
+};
 
-
-exports.deleteComment = (req, res) => {
-
-}
+exports.deleteComment = (req, res) => {};
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const auth = require('../middleware/auth')
-const commentCtrl = require('../controllers/comments')
+const express = require("express");
+const auth = require("../middleware/auth");
+const commentCtrl = require("../controllers/comments");
 const router = express.Router();
-
 
 // Routes
 
-router.post('/newComment', commentCtrl.createComment)
+router.post("/:id/newComment", auth, commentCtrl.createComment);
+router.get("/getComments", commentCtrl.getComment);
+router.delete("/delete", auth, commentCtrl.deleteComment);
 
-
-module.exports = router
+module.exports = router;
