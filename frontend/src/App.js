66876a307b9345a25277f00d50dd37e0cfda8553
Hilderal Térence <terencehilderal@gmail.com>
2020-11-12 22:22:20 +0100

    routes privées ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,23 @@ import SignUp from "./components/SignUp";
 import Profile from "./components/Profile";
 import Post from "./components/Posts";
 import axios from "axios";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import UserContext from "./components/Context";
 
 const token = localStorage.getItem("token");
 const profile = JSON.parse(localStorage.getItem("profile"));
 axios.defaults.headers.common["Authorization"] = token;
+
+const PrivateRoute = ({ component: Component, path }) => {
+	return (
+		<Route
+			exact
+			path={path}
+			render={() => (profile ? <Component /> : <Redirect to="/login" />)}
+		></Route>
+	);
+};
 
 export class App extends Component {
 	render() {
@@ -23,9 +33,9 @@ export class App extends Component {
 					<Route exact path="/" component={SignUp} />
 					<Route exact path="/login" component={LogIn} />
 					<UserContext.Provider value={profile}>
-						<Route exact path="/myprofile/" component={Profile} />
-						<Route exact path="/wall" component={Post} />
-						<Route exact path="/wall/:UserId" component={Post} />
+						<PrivateRoute exact path="/myprofile/" component={Profile} />
+						<PrivateRoute exact path="/wall" component={Post} />
+						<PrivateRoute exact path="/wall/:UserId" component={Post} />
 					</UserContext.Provider>
 					<Footer />
 				</div>
