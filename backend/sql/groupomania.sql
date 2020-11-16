ef34312b58a0bb39f1c4ab8259680dec26f0347e
Hilderal Térence <terencehilderal@gmail.com>
2020-11-16 13:37:56 +0100

    creation du script pour la db

---
+++ backend/sql/groupomania.sql
@@ -0,0 +1,124 @@
+CREATE DATABASE
+IF NOT EXISTS groupomania_development /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE groupomania_development;
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: groupomania_development
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table Comments
+--
+
+DROP TABLE IF EXISTS Comments;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE Comments
+(
+  id int NOT NULL
+  AUTO_INCREMENT,
+  userId int NOT NULL,
+  postId int NOT NULL,
+  comments varchar
+  (255) DEFAULT NULL,
+  createdAt datetime NOT NULL,
+  updatedAt datetime NOT NULL,
+  PRIMARY KEY
+  (id),
+  KEY userId
+  (userId),
+  KEY postId
+  (postId),
+  CONSTRAINT comments_ibfk_1 FOREIGN KEY
+  (userId) REFERENCES Users
+  (id),
+  CONSTRAINT comments_ibfk_2 FOREIGN KEY
+  (postId) REFERENCES Posts
+  (id)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+  --
+  -- Table structure for table Posts
+  --
+
+  DROP TABLE IF EXISTS Posts;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE Posts
+  (
+    id int NOT NULL
+    AUTO_INCREMENT,
+  userId int NOT NULL,
+  title varchar
+    (255) NOT NULL,
+  content varchar
+    (255) NOT NULL,
+  attachment varchar
+    (255) DEFAULT NULL,
+  isModerate tinyint
+    (1) DEFAULT NULL,
+  createdAt datetime NOT NULL,
+  updatedAt datetime NOT NULL,
+  PRIMARY KEY
+    (id),
+  KEY userId
+    (userId),
+  CONSTRAINT posts_ibfk_1 FOREIGN KEY
+    (userId) REFERENCES Users
+    (id)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+    --
+    -- Table structure for table `Users`
+    --
+
+    DROP TABLE IF EXISTS Users;
+    /*!40101 SET @saved_cs_client     = @@character_set_client */;
+    /*!50503 SET character_set_client = utf8mb4 */;
+    CREATE TABLE Users
+    (
+      id int NOT NULL
+      AUTO_INCREMENT,
+  email varchar
+      (255) NOT NULL,
+  username varchar
+      (255) NOT NULL,
+  password varchar
+      (255) NOT NULL,
+  role varchar
+      (255) NOT NULL,
+  isAdmin tinyint
+      (1) DEFAULT NULL,
+  latent tinyint
+      (1) DEFAULT NULL,
+  createdAt datetime NOT NULL,
+  updatedAt datetime NOT NULL,
+  PRIMARY KEY
+      (id)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-16 12:22:41
