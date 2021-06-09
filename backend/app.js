1b7529a17165f3b5d11dcdb61360c9f254c360e1
Hilderal Térence <terencehilderal@gmail.com>
2021-06-09 13:24:36 +0200

    ok

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
-const express = require("express");
-const helmet = require("helmet");
-const bodyParser = require("body-parser");
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
 const app = express();
-const usersRoutes = require("./routes/users");
-const postsRoutes = require("./routes/posts");
-const commentsRoutes = require("./routes/comments");
+const usersRoutes = require('./routes/users');
+const postsRoutes = require('./routes/posts');
+const commentsRoutes = require('./routes/comments');
 
-const path = require("path");
+const path = require('path');
 
 // dotenv
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 /*CROSS ORIGIN RESOURCE SHARING */
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*"); //l'origine qui a le droit d'accéder à notre api = tout le monde
+	res.setHeader('Access-Control-Allow-Origin', '*'); //l'origine qui a le droit d'accéder à notre api = tout le monde
 	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-with, Content, Accept, Content-Type, Authorization"
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-with, Content, Accept, Content-Type, Authorization',
 	); //on autorise certains headers
 	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS',
 	); // on autorise certaines méthod
 	next();
 });
@@ -30,11 +32,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /* HELMET */
 app.use(helmet());
 /*MULTER*/
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // routes
-app.use("/api/users/", usersRoutes);
-app.use("/api/posts/", postsRoutes);
-app.use("/api/posts/", commentsRoutes);
+app.use('/api/users/', usersRoutes);
+app.use('/api/posts/', postsRoutes);
+app.use('/api/posts/', commentsRoutes);
 
 module.exports = app;
